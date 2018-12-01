26bc4233b4099b32948a305f98712a7f700cc29d
kirillstarlight <kirillstarlight@gmail.com>
2018-12-01 04:01:05 +0300

    add name selector

--- index.rb
+++ index.rb
@@ -8,8 +8,8 @@ links = page.xpath("//a[@class='product_img_link product-list-category-img']")
 
 result = links.map do |link| 
     itemPage = agent.click(link)
-    name = ""
-    price =""
-    image = ""
-    puts(itemPage.xpath("//div[@class='attribute_list']"))
+    name = itemPage.xpath("//h1[@class='product_main_name']").text
+    price = itemPage.xpath("//div[@class='attribute_list']")
+    image = itemPage.xpath("//img[@id='bigpic']")
+    puts(name, price, image,"=====================")
 end
