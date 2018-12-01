9113ce0e7f6e7131ed0a1d05dddb1278e08c47e8
kirillstarlight <kirillstarlight@gmail.com>
2018-12-01 06:01:09 +0300

    solve autoscroll problem

--- index.rb
+++ index.rb
@@ -2,15 +2,23 @@ require 'mechanize'
 require 'date'
 
 agent = Mechanize.new
-page  = agent.get("https://www.petsonic.com/hobbit-half/?p=2")
-puts page.title
-links = page.xpath("//a[@class='product_img_link product-list-category-img']")
+startUrl = "https://www.petsonic.com/hobbit-half/"
 
-result = links.map do |link| 
-    itemPage = agent.click(link)
-    name = itemPage.xpath("//h1[@class='product_main_name']").text
-    nameCategory = itemPage.xpath("//span[@class='radio_label']").text
-    price = itemPage.xpath("//span[@class='price_comb']").text
-    image = itemPage.xpath("//img[@id='bigpic']").attr('src')
-    puts(name,nameCategory, price, image)
-end
+page  = agent.get(startUrl)
+startTitle = page.title
+i = 2
+begin
+    puts(i)
+    links = page.xpath("//a[@class='product_img_link product-list-category-img']")
+    result = links.map do |link| 
+        itemPage = agent.click(link)
+        name = itemPage.xpath("//h1[@class='product_main_name']").text
+        nameCategory = itemPage.xpath("//span[@class='radio_label']").text
+        price = itemPage.xpath("//span[@class='price_comb']").text
+        image = itemPage.xpath("//img[@id='bigpic']").attr('src')
+        # puts(name,nameCategory, price, image)
+    end
+    newPage = startUrl+"?p=" + i.to_s
+    page = agent.get(newPage)
+    i += 1
+end while startTitle!=page.title
