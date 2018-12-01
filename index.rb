1153845547f09fb4ea5b170db46156289b9f52f4
kirillstarlight <kirillstarlight@gmail.com>
2018-12-01 06:55:20 +0300

    complete

--- index.rb
+++ index.rb
@@ -1,24 +1,31 @@
 require 'mechanize'
-require 'date'
+require 'csv'
 
 agent = Mechanize.new
-startUrl = "https://www.petsonic.com/hobbit-half/"
 
+startUrl = ARGV[0]
 page  = agent.get(startUrl)
 startTitle = page.title
-i = 2
-begin
-    puts(i)
-    links = page.xpath("//a[@class='product_img_link product-list-category-img']")
-    result = links.map do |link| 
-        itemPage = agent.click(link)
-        name = itemPage.xpath("//h1[@class='product_main_name']").text
-        nameCategory = itemPage.xpath("//span[@class='radio_label']").text
-        price = itemPage.xpath("//span[@class='price_comb']").text
-        image = itemPage.xpath("//img[@id='bigpic']").attr('src')
-        # puts(name,nameCategory, price, image)
-    end
-    newPage = startUrl+"?p=" + i.to_s
-    page = agent.get(newPage)
-    i += 1
-end while startTitle!=page.title
+pageNumber = 2
+
+CSV.open(ARGV[1],"w") do |csv|
+    begin
+        links = page.xpath("//a[@class='product_img_link product-list-category-img']")
+        result = links.map do |link| 
+            itemPage = agent.click(link)
+            currentRecord = []
+            name = itemPage.xpath("//h1[@class='product_main_name']").text
+            itemPage.xpath("//span[@class='radio_label']").map do |category|
+                i = 0
+                currentRecord.push(name + " " + category.text)
+                currentRecord.push(itemPage.xpath("//span[@class='price_comb']")[i].text)
+                currentRecord.push(itemPage.xpath("//img[@id='bigpic']").attr('src'))
+                i+=1 
+            end
+            csv << currentRecord
+        end
+        newPage = startUrl+"?p=" + pageNumber.to_s
+        page = agent.get(newPage)
+        pageNumber += 1
+    end while startTitle!=page.title
+end
